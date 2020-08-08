c2b4506092fdee3339fc9de33712322802704b6c
Lance Page <31941666+6ftdotexe@users.noreply.github.com>
2020-08-07 20:53:15 -0500

    Local Working Directory pls work on Heroku

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,24 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "page2110",
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'page2110',
+    database: 'burgers_db'
+  });
+};
+
 // Make connection
-connection.connect(function(err) {
-  if(err) throw err;
+connection.connect(function (err) {
+  if (err) throw err;
   console.log("Connected as id: " + connection.threadId);
 })
 
